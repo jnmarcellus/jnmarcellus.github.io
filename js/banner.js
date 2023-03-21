46103f8f9d87522d62a3dfc2bf56b4a3e3bd586b
jnmarcellus <jnmarcellus@users.noreply.github.com>
2023-03-21 13:16:36 -0500

    Add files via upload

---
+++ js/banner.js
@@ -0,0 +1,36 @@
+const getTopStories = async () => {
+    const response = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json");
+    const storyIds = await response.json();
+    return storyIds.slice(0, 10);
+};
+
+const getStory = async (storyId) => {
+    const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`);
+    const story = await response.json();
+    return story;
+};
+
+const displayBanner = async (stories) => {
+    const bannerContainer = document.getElementById("banner-container");
+    let currentStoryIndex = 0;
+
+    const updateBanner = () => {
+        const story = stories[currentStoryIndex];
+        bannerContainer.innerHTML = `
+            <h2><a href="${story.url}" target="_blank">${story.title}</a></h2>
+            <p>By: ${story.by} | Score: ${story.score}</p>
+        `;
+        currentStoryIndex = (currentStoryIndex + 1) % stories.length;
+    };
+
+    setInterval(updateBanner, 3000); // Change the duration (in milliseconds) to control the rotation speed
+    updateBanner();
+};
+
+const fetchAndDisplayStories = async () => {
+    const topStoryIds = await getTopStories();
+    const topStories = await Promise.all(topStoryIds.map(getStory));
+    displayBanner(topStories);
+};
+
+fetchAndDisplayStories();

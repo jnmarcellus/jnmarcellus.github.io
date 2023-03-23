0c17b8e2a59e5ab0e49738f2e6a39f635378503e
jnmarcellus <jnmarcellus@users.noreply.github.com>
2023-03-23 06:04:31 -0500

    Update stock.js

--- js/stock.js
+++ js/stock.js
@@ -1,27 +1,31 @@
-$(document).ready(function() {
-  var symbols = ["AAPL", "MSFT", "AMZN", "GOOGL", "FB", "TSLA"]; // Array of stock symbols
-  var index = 0; // Index to keep track of current stock symbol
+// Define a function that updates the stock quote
+const updateStockQuote = () => {
+	const API_KEY = "e4e528bb32eb4bc59f2c3bd5fe304ebb";
+	const SYMBOLS = ["AAPL", "MSFT", "AMZN", "GOOGL", "FB", "TSLA"];
+	const symbol = SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)];
+	const url = `https://api.twelvedata.com/quote?symbol=${symbol}&apikey=${API_KEY}`;
 
-  // Function to get stock data and update banner
-  function updateBanner() {
-    var symbol = symbols[index];
-    var url = "https://api.twelvedata.com/quote?symbol=" + symbol + "&apikey=e4e528bb32eb4bc59f2c3bd5fe304ebb";
+	fetch(url)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(`API request failed: ${response.status} ${response.statusText}`);
+			}
+			return response.json();
+		})
+		.then(data => {
+			const price = parseFloat(data.close).toFixed(2);
+			const change = parseFloat(data.change).toFixed(2);
+			const changePercent = parseFloat(data.percent_change).toFixed(2);
+			const html = `<strong>${symbol}</strong>: ${price} (${change}, ${changePercent}%)`;
+			document.getElementById("stock-quote").innerHTML = html;
+		})
+		.catch(error => {
+			console.error(error);
+		});
+};
 
-    $.getJSON(url, function(data) {
-      var price = data.close;
-      var change = data.change;
-      var changePercent = data.percent_change;
-      var html = "<strong>" + symbol + "</strong>: " + price.toFixed(2) + " (" + change.toFixed(2) + ", " + changePercent.toFixed(2) + "%)";
-      $("#stock-quote").html(html);
-    });
+// Call the updateStockQuote function when the page loads
+window.onload = updateStockQuote;
 
-    // Increment index and cycle through symbols array
-    index++;
-    if (index == symbols.length) {
-      index = 0;
-    }
-  }
-
-  // Update banner every 5 seconds
-  setInterval(updateBanner, 5000);
-});
+// Call the updateStockQuote function every 5 seconds
+setInterval(updateStockQuote, 5000);
